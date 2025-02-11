6ef6c0ba89ad7dce3e7601a83327afbca8fea2b1
Ashish Kumar <ashishkumar901336@gmail.com>
2025-02-11 23:12:44 +0530

    feat: cat implemented -n flag

--- cat/cat.go
+++ cat/cat.go
@@ -1,39 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	files := os.Args[1:]
+	nFlag := flag.Bool("n", false, "Number all output lines")
+	flag.Parse()
+
+	files := flag.Args()
 
 	if len(files) == 0 {
-		_, err := io.Copy(os.Stdout, os.Stdin)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cat: error reading stdin: %v\n", err)
-			os.Exit(1)
-		}
+		printStream(os.Stdin, *nFlag)
 		return
 	}
 
-	for _, file := range files {
-		err := printFile(file)
-		if err != nil {
+	for i, file := range files {
+		if err := printFile(file, *nFlag); err != nil {
 			fmt.Fprintf(os.Stderr, "cat: %s: %v\n", file, err)
 			os.Exit(1)
+		}
+
+		if i < len(files)-1 {
+			fmt.Println()
 		}
 	}
 }
 
-func printFile(filename string) error {
+func printFile(filename string, numberLines bool) error {
 	f, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	_, err = io.Copy(os.Stdout, f)
-	return err
+	printStream(f, numberLines)
+	return nil
+}
+
+func printStream(r io.Reader, numberLines bool) {
+	if numberLines {
+		scanner := bufio.NewScanner(r)
+		lineNum := 1
+		for scanner.Scan() {
+			fmt.Printf("%6d  %s\n", lineNum, scanner.Text())
+			lineNum++
+		}
+	} else {
+		_, _ = io.Copy(os.Stdout, r)
+		fmt.Println()
+	}
 }
